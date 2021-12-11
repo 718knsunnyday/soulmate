311663d0c73f731aa13c44b22af75a2b4e87609f
718knsunnyday <718mickeeey@gmail.com>
2021-12-11 08:05:42 +0000

    [Update]いいね機能

--- app/controllers/farmer/favorites_controller.rb
+++ app/controllers/farmer/favorites_controller.rb
@@ -1,16 +1,16 @@
 class Farmer::FavoritesController < ApplicationController
-  before_action :authenticate_public!
-  before_action :authenticate_farmer!
+  #before_action :authenticate_public!
+  #before_action :authenticate_farmer!
 
   def create
     @farm = Farm.find(params[:farm_id])
-    favorite = @farm.favorites.new(public_id: current_public)
+    favorite = @farm.favorites.new(public_id: current_public.id)
     favorite.save
   end
 
   def destroy
     @farm = Farm.find(params[:farm_id])
-    favorite = @farm.favorites.find_by(public_id: current_public)
+    favorite = @farm.favorites.find_by(public_id: current_public.id)
     favorite.destroy
   end
 end
