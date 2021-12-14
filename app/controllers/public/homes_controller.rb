a86c15dc8897f54277d8e07b78f88a10b8079fea
718knsunnyday <718mickeeey@gmail.com>
2021-12-14 12:37:51 +0000

    [Update]複数検索機能

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,13 +1,13 @@
 class Public::HomesController < ApplicationController
-  
+
   def top
   end
-  
+
   def about
   end
-  
+
   def search
     @farms = Farm.search(params[:search])
   end
-  
+
 end
--- app/models/farm.rb
+++ app/models/farm.rb
@@ -2,15 +2,15 @@ class Farm < ApplicationRecord
   attachment :image
   has_many :favorites, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
-  
+
   def self.search(search)
-    Farm.where(['name LIKE ?', "%#{search}%"])
+    Farm.where(['name LIKE ? OR prefecture LIKE ? OR city LIKE? OR variety LIKE? OR breed LIKE?', "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%"])
   end
 
   def favorited_by?(farm)
     favorites.where(farm_id: farm.id).exists?
   end
-  
+
   def bookmarked_by?(farm)
     bookmarks.where(farm_id: farm.id).exists?
   end
