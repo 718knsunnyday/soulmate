81978f59ebbcfb1ecf0736433bbbffe346df9c16
718knsunnyday <718mickeeey@gmail.com>
2021-12-26 13:58:16 +0000

    [Update]RSpec

--- spec/system/top_spec.rb
+++ spec/system/top_spec.rb
@@ -68,26 +68,7 @@ describe 'トップページのテスト' do
     end
   end
 
-  # describe 'ユーザー新規登録・ログインのテスト' do
-  #   let!(:public_user) { build(:public) }
-  #   before do
-  #     new_public_registration_path
-  #     fill_in 'public[last_name]', with: public_user.last_name
-  #     fill_in 'public[first_name]', with: public_user.first_name
-  #     fill_in 'public[last_name_kana]', with: public_user.last_name_kana
-  #     fill_in 'public[first_name_kana]', with: public_user.first_name_kana_name
-  #     fill_in 'public[email]', with: public_user.email
-  #     fill_in 'public[password]', with: public_user.password
-  #     fill_in 'public[password_confirmation]', with: public_user.password_confirmation
-  #     click_button '新規登録する'
-  #   end
-  #   it '新規登録成功後のマイページへ' do
-  #     expect(page).to have_current_path customer_path(Public.last)
-  #   end
-  # end
-  
-  describe 'ユーザーのテスト' do
-    context 'ユーザー新規登録後、マイページのテスト' do
+  describe 'ユーザー新規登録・ログインのテスト' do
     let(:public_user) { build(:public) }
     before do
       visit new_public_registration_path
@@ -97,15 +78,10 @@ describe 'トップページのテスト' do
       fill_in 'public[first_name_kana]', with: public_user.first_name_kana
       fill_in 'public[email]', with: public_user.email
       fill_in 'public[password]', with: public_user.password
-      fill_in 'public[password_confirmation]', with: public_user.password
+      fill_in 'public[password_confirmation]', with: public_user.password_confirmation
       click_button '新規登録する'
     end
-      it 'ユーザー新規登録成功後登録した内容を表示' do
-        expect(page).to have_content public_user.last_name
-        expect(page).to have_content public_user.first_name
-        expect(page).to have_content public_user.last_name_kana
-        expect(page).to have_content public_user.first_name_kana
-        expect(page).to have_content public_user.email
-      end
+    it '新規登録成功後のマイページへ' do
+      expect(page).to have_current_path customer_path(Public.last)
     end
   end
