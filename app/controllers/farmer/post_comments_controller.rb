bc21c81004aed0fbac781638598be7eb716464ea
718knsunnyday <718mickeeey@gmail.com>
2021-12-29 12:53:05 +0000

    [Fix]コメント機能

--- app/controllers/farmer/post_comments_controller.rb
+++ app/controllers/farmer/post_comments_controller.rb
@@ -4,18 +4,12 @@ class Farmer::PostCommentsController < ApplicationController
     @post_comment = PostComment.new(post_comment_params)
     @post_comment.public_id = current_public.id
     @post_comment.farm_id = @farm.id
-    if @post_comment.save
-      flash.now[:notice] = 'コメントを投稿しました。'
-    else
-      render :show
-    end
+    @post_comment.save
   end
 
   def destroy
     @farm = Farm.find(params[:farm_id])
     @post_comment = PostComment.find_by(id: [params[:id]]).destroy
-      flash.now[:alert] = '投稿を削除しました。'
-    render :destroy
   end
 
   private
