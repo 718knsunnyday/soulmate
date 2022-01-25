2a3f47c9db3b6aad8579549c57e88c01f4406018
718knsunnyday <718mickeeey@gmail.com>
2022-01-25 02:58:20 +0000

    [Fix]検索機能

--- app/controllers/farmer/post_comments_controller.rb
+++ app/controllers/farmer/post_comments_controller.rb
@@ -1,15 +1,15 @@
 class Farmer::PostCommentsController < ApplicationController
   def create
     @farm = Farm.find(params[:farm_id])
-    @post_comment = PostComment.new(post_comment_params)
-    @post_comment.public_id = current_public.id
-    @post_comment.farm_id = @farm.id
-    @post_comment.save
+    post_comment = PostComment.new(post_comment_params)
+    post_comment.public_id = current_public.id
+    post_comment.farm_id = @farm.id
+    post_comment.save
   end
 
   def destroy
     @farm = Farm.find(params[:farm_id])
-    @post_comment = PostComment.find_by(id: [params[:id]]).destroy
+    post_comment = PostComment.find_by(id: [params[:id]]).destroy
   end
 
   private
--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -7,6 +7,11 @@ class Public::HomesController < ApplicationController
   end
 
   def search
-    @farms = Farm.search(params[:search]).order(params[:sort]).page(params[:page]).per(5)
+    if params[:search].present?
+      @farms = Farm.search(params[:search]).order(params[:sort]).page(params[:page]).per(5)
+    else
+      @farms = Farm.order(created_at: :desc).limit(4)
+      redirect_to root_path
+    end
   end
 end
